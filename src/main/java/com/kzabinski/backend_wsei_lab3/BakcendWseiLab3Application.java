5dac82e0dd1e9a54d199872bba23179d2b3743cb
Krzysztof Żabiński <krzysztofzabinski@Krzysztofs-MacBook-Air.local>
2025-03-21 12:59:36 +0100

    Removing unused import

--- src/main/java/com/kzabinski/backend_wsei_lab3/BakcendWseiLab3Application.java
+++ src/main/java/com/kzabinski/backend_wsei_lab3/BakcendWseiLab3Application.java
@@ -2,7 +2,6 @@ package com.kzabinski.backend_wsei_lab3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class BakcendWseiLab3Application {
